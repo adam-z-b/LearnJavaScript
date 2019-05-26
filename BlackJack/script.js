a0c2ce1c12b6b273aeb7b7037be5a7bb38f73c67
Adam Zvi Borenstein <Adam.Zvi.Borenstein@gmail.com>
2019-05-26 23:28:19 +0300

    end of day

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -14,7 +14,20 @@ let textArea = document.getElementById( "text-area" );
 let newGameButton = document.getElementById( "newGame-button" );
 let hitButton = document.getElementById( "hit-button" );
 let stayButton = document.getElementById( "stay-button" );
+
+let gameStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    deck = [];
 InitGame();
+
+function GetCardString( card ) {
+  return card.value + " of " + card.suit;
+}
 
 function InitGame() {
   HideButtons();
@@ -27,7 +40,28 @@ function AddEvenHandlers() {
     newGameButton.style.display = "none";
     hitButton.style.display = "inline";
     stayButton.style.display = "inline";
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+    deck = CreateDeck();
+    ShuffleDeck( deck );
+    dealerCards = [ getNextCard(), getNextCard() ];
+    playerCards = [ getNextCard(), getNextCard() ];
+    ShowStatus();
   });
+}
+
+function ShuffleDeck( deck ) {
+  for ( let index = 0; i < array.length; ++index ) {
+    let swapIdx = Math.trunc( Math.random() * deck.length ) ;
+    let tmpCard = deck[ swapIdx ];
+    deck[ swapIdx ] = deck[ index ];
+    deck[ index ] = temCard;
+  }
+}
+
+function SwapCards( card, card, number ) {
+
 }
 function HideButtons() {
   hitButton.style.display = "none";
@@ -48,23 +82,10 @@ function CreateDeck() {
   }
   return tempDeck;
 }
-// deck of cards,
-let deck = CreateDeck();
 
-function GetNextCard() {
-  return deck.shift();
+function ShowStatus() {
+  if ( !gameStarted ) {
+    textArea.innerText = "Wellcome to Blackjack.";
+    return;
+  }
 }
-
-let playerCards = [
-  GetNextCard(),
-  GetNextCard()
-];
-
-function GetCardString( card ) {
-  return card.value + " of " + card.suit;
-}
-
-console.log( "Welcome to Blackjack!" );
-console.log( "You are dealt:" );
-console.log( " " + GetCardString( playerCards[0] ) );
-console.log( " " + GetCardString( playerCards[1] ) )
