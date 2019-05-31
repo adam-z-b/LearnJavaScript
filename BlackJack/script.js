f443b006a782cc87c5ca3330a97b3a8f4e983db2
Adam Zvi Borenstein <Adam.Zvi.Borenstein@gmail.com>
2019-05-31 15:37:10 +0300

    Added display for dealer and player decks after game starts

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -23,6 +23,7 @@ let gameStarted = false,
     dealerScore = 0,
     playerScore = 0,
     deck = [];
+
 InitGame();
 
 function GetCardString( card ) {
@@ -31,10 +32,10 @@ function GetCardString( card ) {
 
 function InitGame() {
   HideButtons();
-  AddEvenHandlers();
+  AddEventHandlers();
 }
 
-function AddEvenHandlers() {
+function AddEventHandlers() {
   newGameButton.addEventListener( "click", function () {
     textArea.innerText = "Started...";
     newGameButton.style.display = "none";
@@ -44,25 +45,34 @@ function AddEvenHandlers() {
     gameOver = false;
     playerWon = false;
     deck = CreateDeck();
+    // console.log( deck[0].value +" of " + deck[0].suit + "\n" );
     ShuffleDeck( deck );
-    dealerCards = [ getNextCard(), getNextCard() ];
-    playerCards = [ getNextCard(), getNextCard() ];
+    // console.log( deck[0].value +" of " + deck[0].suit + "\n" );
+    dealerCards = [ GetNexCard(), GetNexCard() ];
+    // console.log( dealerCards[0].value +" of " + dealerCards[0].suit + "\n" );
+    playerCards = [ GetNexCard(), GetNexCard() ];
+    // console.log( playerCards[0].value +" of " + playerCards[0].suit + "\n" );
     ShowStatus();
   });
 }
 
+function GetNexCard() {
+  let card = deck.shift();
+  return card;
+}
 function ShuffleDeck( deck ) {
-  for ( let index = 0; i < array.length; ++index ) {
+  for ( let index = 0; index < deck.length; ++index ) {
     let swapIdx = Math.trunc( Math.random() * deck.length ) ;
     let tmpCard = deck[ swapIdx ];
     deck[ swapIdx ] = deck[ index ];
-    deck[ index ] = temCard;
+    deck[ index ] = tmpCard;
   }
 }
 
 function SwapCards( card, card, number ) {
 
 }
+
 function HideButtons() {
   hitButton.style.display = "none";
   stayButton.style.display = "none";
@@ -77,6 +87,7 @@ function CreateDeck() {
         suit: suits[suitIdx],
         value: values[valuesIdx]
       };
+      // console.log( "Adding card " + card.value + " of " + card.suit );
       tempDeck.push( card );
     }
   }
@@ -88,4 +99,48 @@ function ShowStatus() {
     textArea.innerText = "Wellcome to Blackjack.";
     return;
   }
+
+  let dealerCardsString = GetCardsString( dealerCards );
+  console.log( dealerCardsString );
+  let playerCardsString = GetCardsString( playerCards );
+
+  UpdateScores();
+
+  textArea.innerText =
+        "Dealer has:\n" +
+        dealerCardsString +
+        "\n\n" +
+        "Player has:\n"  +
+        playerCardsString +
+        "\n\n"
+        if ( gameOver ) {
+          if ( playerWon ) {
+            textArea.innerText = "You Win. WTG.\n";
+          } else {
+            textArea.innerText = "Dealer Won.\nBetter luck netx time...\n";
+          }
+          newGameButton.style.display = "inline";
+          newGameButton.style.innerText = "Start another game.";
+          hitButton.style.display = "none";
+          stayButton.style.display = "none";
+        }
 }
+
+function UpdateScores() {
+
+}
+
+function GetCardsString( cards ) {
+  let cardsString = "";
+  for (var i = 0; i < cards.length; i++) {
+    cardsString += GetCardString( cards[i] ) + "\n";
+  }
+  return cardsString;
+}
+
+// function GetCardsString( card ) {
+//   let cardsString = "";
+//   for ( let i = 0; i < cards.length; ++i ) {
+//     cardsString += cards[i] + "\n";
+//   }
+// }
